a319a08a89658742835071e9cc50ff95659c2604
David Read <david.read@hackneyworkshop.com>
2018-10-08 10:43:28 +0100

    Add logging

--- unidler.py
+++ unidler.py
@@ -26,7 +26,7 @@ UNIDLER = 'unidler'
 UNIDLER_NAMESPACE = 'default'
 
 logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'DEBUG'))
-log = logging.getLogger('unidler')
+app_log = logging.getLogger('unidler')
 logging.getLogger('kubernetes').setLevel(logging.WARNING)
 
 
@@ -37,7 +37,7 @@ def run(host='0.0.0.0', port=8080):
         config.load_kube_config()
 
     unidler = UnidlerServer((host, port), RequestHandler)
-    log.info(f'Unidler listening on {host}:{port}')
+    app_log.info(f'Unidler listening on {host}:{port}')
     unidler.serve_forever()
 
 
@@ -53,19 +53,29 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         hostname = self.headers.get('Host', UNIDLER)
         if hostname.startswith(UNIDLER):
-            log.debug('No hostname specified')
+            app_log.debug('No hostname specified')
             self.respond(HTTPStatus.NO_CONTENT, '')
             return
 
+        username = hostname.split('.')[0]
+        log = logging.getLogger('unidler:{}'.format(username))
+
         try:
             if hostname in self.unidling:
+                log.debug('Internal state: unidling is in progress')
                 if self.unidling[hostname].is_done():
                     self.unidling[hostname].enable_ingress()
                     del self.unidling[hostname]
+                else:
+                    log.debug('Unidling is not done yet')
 
             elif is_idle(hostname):
-                self.unidling[hostname] = Unidling(hostname)
+                log.debug('It is idle, so starting unidling')
+                self.unidling[hostname] = Unidling(hostname, log)
                 self.unidling[hostname].start()
+
+            else:
+                log.error('BAD STATE 4')
 
         except (DeploymentNotFound, IngressNotFound) as not_found:
             self.send_error(HTTPStatus.NOT_FOUND, str(not_found))
@@ -93,48 +103,59 @@ class IngressNotFound(Exception):
 
 class Unidling(object):
 
-    def __init__(self, hostname):
+    def __init__(self, hostname, log=app_log):
         self.hostname = hostname
         self.ingress = None
         self.deployment = None
         self.started = False
         self.replicas = 0
         self.enabled = False
+        self.log = log
 
     def start(self):
         if not self.started:
+            self.log.debug('Starting unidle')
             self.started = True
             self.ingress = ingress_for_host(self.hostname)
             self.deployment = deployment_for_ingress(self.ingress)
 
-            restore_replicas(self.deployment)
-            unmark_idled(self.deployment)
+            restore_replicas(self.deployment, self.log)
+            unmark_idled(self.deployment, self.log)
 
             # XXX writing changes triggers the asynchronous creation of
             # pods, which can take a few seconds
-            write_deployment_changes(self.deployment)
+            write_deployment_changes(self.deployment, self.log)
+        else:
+            self.log.debug('BAD STATE 1')
 
     def is_done(self):
         if self.started:
             self.deployment = deployment_for_ingress(self.ingress)
             replicas = int(self.deployment.status.available_replicas or 0)
+            self.log.debug('Is done?\n  "idled" label removed = {}\n  replicas = {}'.format(
+                IDLED not in self.deployment.metadata.labels, replicas))
             return (
                 IDLED not in self.deployment.metadata.labels and
                 replicas >= 1)
+        else:
+            self.log.debug('BAD STATE 2')
         return False
 
     def enable_ingress(self):
         if not self.enabled:
             self.enabled = True
+            self.log.debug('Enabling ingress')
             ingress = unidler_ingress()
-            remove_host_rule(self.hostname, ingress)
-            write_ingress_changes(ingress)
+            remove_host_rule(self.hostname, ingress, self.log)
+            write_ingress_changes(ingress, self.log)
             # XXX do we need to wait here for the ingress controller to pick up the
             # changes?
             ingress = ingress_for_host(self.hostname)
             enable_ingress(self.ingress)
-            write_ingress_changes(self.ingress)
-
+            write_ingress_changes(self.ingress, self.log)
+        else:
+            self.log.debug('BAD STATE 3')
+        
 
 def deployment_for_ingress(ingress):
     try:
@@ -142,7 +163,7 @@ def deployment_for_ingress(ingress):
             ingress.metadata.name,
             ingress.metadata.namespace)
 
-    except kubernetes.client.rest.ApiException as error:
+    except kubernetes.client.rest.ApiException:
         raise DeploymentNotFound(
             ingress.metadata.name,
             ingress.metadata.namespace)
@@ -166,12 +187,13 @@ def ingress_for_host(hostname):
     return ingress
 
 
-def is_idle(hostname):
+def is_idle(hostname, log=app_log):
     deployment = deployment_for_ingress(ingress_for_host(hostname))
+    log.debug('Is idle?  "idled" label = {}'.format(IDLED in deployment.metadata.labels))
     return IDLED in deployment.metadata.labels
 
 
-def restore_replicas(deployment):
+def restore_replicas(deployment, log=app_log):
     annotation = deployment.metadata.annotations.get(IDLED_AT)
 
     if annotation is not None:
@@ -183,8 +205,7 @@ def restore_replicas(deployment):
         # TODO Assume a default of 1?
         log.error('Deployment has no idled-at annotation')
 
-
-def unmark_idled(deployment):
+def unmark_idled(deployment, log=app_log):
     log.debug('Removing idled annotation and label')
     if IDLED in deployment.metadata.labels:
         del deployment.metadata.labels[IDLED]
@@ -192,7 +213,7 @@ def unmark_idled(deployment):
         del deployment.metadata.annotations[IDLED_AT]
 
 
-def write_deployment_changes(deployment):
+def write_deployment_changes(deployment, log=app_log):
     log.debug(
         f'Writing changes to deployment {deployment.metadata.name} '
         f'in namespace {deployment.metadata.namespace}')
@@ -202,7 +223,7 @@ def write_deployment_changes(deployment):
         deployment)
 
 
-def write_ingress_changes(ingress):
+def write_ingress_changes(ingress, log=app_log):
     log.debug(
         f'Writing changes to ingress {ingress.metadata.name} '
         f'in namespace {ingress.metadata.namespace}')
@@ -217,15 +238,18 @@ def unidler_ingress():
         UNIDLER, UNIDLER_NAMESPACE)
 
 
-def remove_host_rule(hostname, ingress):
+def remove_host_rule(hostname, ingress, log=app_log):
     log.debug(
         f'Removing host rules for {hostname} '
         f'from ingress {ingress.metadata.name} '
         f'in namespace {ingress.metadata.namespace}')
+    
+    num_rules_before = len(ingress.spec.rules)
     ingress.spec.rules = list(
         filter(
             lambda rule: rule.host != hostname,
             ingress.spec.rules))
+    log.debug('Rules removed: {}'.format(num_rules_before - len(ingress.spec.rules)))
 
 
 def enable_ingress(ingress):
